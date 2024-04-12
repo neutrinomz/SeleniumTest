6037b7da898bd9d137c25db35d51b6e84d09e8c3
neutrinoez <trstilmax24@gmail.com>
2024-04-13 03:34:14 +0500

    test

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Seleniumtest_zubkov;
+    
+public class Seleniumtest
+{
+    [Test]
+    public void Authorisation()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maxinized", "--disable-extensions");
+        var driver = new ChromeDriver(options);
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
+        Thread.Sleep(3000);
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("zubkoff.m@yandex.ru");
+        var password = driver.FindElement(By.Id("Password"));
+        password.SendKeys("41260Trat31193.");
+        Thread.Sleep(3000);
+        var button = driver.FindElement(By.Name("button"));
+        button.Click();
+        Thread.Sleep(3000);
+        var currenturl = driver.Url;
+        Assert.That(currenturl == "https://staff-testing.testkontur.ru/news");
+        driver.Quit();
+    }
+}
